96a0cca6e393df93c0d97f3475d9130316e01aec
Shubham kumar <krantimaurya9523@gmail.com>
2026-01-23 22:56:58 +0530

    chore: setup MongoDB atlas connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,18 @@
 import express from "express"
+import mongoose from "mongoose"
 import dotenv from "dotenv"
 const app = express()
 dotenv.config();
 
 const PORT = process.env.PORT
+const DB_URI=process.env.MongoDB_URI
+
+try {
+    mongoose.connect(DB_URI);
+    console.log("MongoDb connected successfully")
+} catch (error) {
+   console.log(error)
+}
 
 app.get('/', (req, res) => {
   res.send('Hello guys')
